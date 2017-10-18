b2176726ec7fc3e75505e43e584efdf57aa71ae5
Josh Riddlestone <jo5hman@mail.com>
2017-10-18 19:03:58 +1300

    new particles and remake last level for sp

--- Golf game 2D/Assets/Scripts/SpawnScript.cs
+++ Golf game 2D/Assets/Scripts/SpawnScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpawnScript : MonoBehaviour {
 
@@ -32,6 +33,9 @@ public class SpawnScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
 }
